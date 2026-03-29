0499fcd4ea674b2af2d5a3b37ab07fdb484dda05
80hkim <80hkim@gmail.com>
2026-03-29 13:47:19 +0900

    Auto update

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ const G = {
     fontSize: 0.022,
   },
   ipad: {
-    padTop: 0.0915,
+    padTop: 0.098,
     padBottom: 0.1138,
     padLeftOdd: 0.301,
     padRightOdd: 0.088,
     padLeftEven: 0.176,
     padRightEven: 0.213,
-    lineHeight: 0.05156,
+    lineHeight: 0.0493,
     fontSize: 0.022,
   }
 };
